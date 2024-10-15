52c25483de3d8fa4db88816bce4c267938fd3a57
Martina Molina <molinamartina57@gmail.com>
2024-10-15 17:28:50 -0300

    index y base

---
+++ vete/scripts/main.js
@@ -0,0 +1,10 @@
+// cambiar la imagen al seleccionarla con el mouse
+let miImage = document.querySelector("img");
+miImage.onclick = function () {
+  let miSrc = miImage.getAttribute("src");
+  if (miSrc === "images/firefox-icon.png") {
+    miImage.setAttribute("src", "images/firefox2.png");
+  } else {
+    miImage.setAttribute("src", "images/firefox-icon.png");
+  }
+};
